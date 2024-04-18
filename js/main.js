618b8715313547b3869d11222dea999f661c5655
Edwin1115 <163002162+nanocd345@users.noreply.github.com>
2024-04-18 22:04:24 +0000

    feat: :sparkles: seguimiento ejercicios 01

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 import prompt from 'async-prompt'
 import sumarRango from "./modules/#8_sumarRangoDeNumeros.js"
-import { numerosAPalabras } from './modules/#27_traducirNumerosAPalabras.js'
-import { log } from 'console';
+import numerosAPalabras from './modules/#27_traducirNumerosAPalabras.js'
+import duplicarArreglo from '.modules/#23_duplicarElementosDeArreglo'
+
+//23. Duplicar elementos de arreglo
+//
+console.log(duplicarArreglo([1,2,3]));
 
 //27. Traducir números a palabras 
 //
-let lista = [];
-let bandera = undefined
-do{
-    lista.push(Number(await prompt("Ingrese un numero: ")))
-    bandera (await prompt("Desea ingresar otro numero?\n\t0.no\n\t1.si"))
-}while (bandera)
-
-console.log(numerosAPalabras());
+//let lista = [];
+//let bandera = undefined
+//do{
+//    lista.push(Number(await prompt("Ingrese un numero: ")))
+//    bandera (await prompt("Desea ingresar otro numero?\n\t0.no\n\t1.si"))
+//}while (bandera)
+//
+//console.log(numerosAPalabras());
 
 //8. Sumar rango de numeros
 //
--- js/modules/#23_duplicarElementosDeArreglo.js
+++ js/modules/#23_duplicarElementosDeArreglo.js
@@ -1,6 +1,4 @@
-function duplicar(arr){
-    let new_rr = arr.map(num => num*2);
+export function duplicarArreglo(arr) {
+    let new_rr = arr.map(num => num * 2);
     return new_rr
 }
-
-console.log(duplicar([1,2,3]));
